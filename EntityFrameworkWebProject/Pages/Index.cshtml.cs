ac230b364c5d016493c62704bd621122731146d4
jcrowe91 <jcrowe9119@gmail.com>
2022-09-27 23:32:32 -0500

    Added sample data via generated.json and added LoadSampleData method

--- EntityFrameworkWebProject/Pages/Index.cshtml.cs
+++ EntityFrameworkWebProject/Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 namespace EntityFrameworkWebProject.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
 
         }
+
+        private void LoadSampleData()
+        {
+            if (_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
+        }
+
+
     }
 }
